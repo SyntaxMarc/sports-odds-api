28e994318b842b6f5394f96c9f60e0ef1eb663f7
Marc Anthony I. Pala <marcpala9@gmail.com>
2025-09-28 15:35:55 +0800

    Update odds.py

--- api/routes/odds.py
+++ api/routes/odds.py
@@ -1,1 +1,12 @@
+from fastapi import APIRouter
+from api.utils.fetch_data import get_odds
 
+router = APIRouter(prefix="/odds", tags=["Odds"])
+
+@router.get("/mlb")
+async def mlb_odds():
+    return await get_odds(sport="baseball_mlb")
+
+@router.get("/nba")
+async def nba_odds():
+    return await get_odds(sport="basketball_nba")
